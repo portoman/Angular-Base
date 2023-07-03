4748cb7b397b743f740c96c65c6e07136765be18
portobuj <portbuj@gmail.com>
2023-07-03 20:00:43 +0200

    Avance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Componentes que formarán las rutas
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { ErrorComponent } from './components/error/error.component';
+
+//Array de rutas
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: 'peliculas', component: PeliculasComponent },
+  { path: 'pagina-de-pruebas', component: PaginaComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +23,12 @@ import { FooterComponent } from './components/footer/footer.component';
     HeaderComponent,
     SliderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    FormularioComponent,
+    PaginaComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
