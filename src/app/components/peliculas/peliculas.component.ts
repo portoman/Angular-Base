6ae7441fba7a1b7f4d698fde9de243bc0984a5f0
portb <aporto@vansia.es>
2023-07-04 18:39:52 +0200

    Avance

--- src/app/components/peliculas/peliculas.component.ts
+++ src/app/components/peliculas/peliculas.component.ts
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
 })
 export class PeliculasComponent {
   public texto: string;
+  public peliculas:Array<any>;
 
   constructor() {
     this.texto = 'hola que tal';
+    this.peliculas=[
+      {title: "Star Wars IV", image:"https://m.media-amazon.com/images/I/81aA7hEEykL._AC_UF1000,1000_QL80_.jpg"},
+      {title: "Star Wars V", image:"https://m.media-amazon.com/images/I/91eOgodm4nL._AC_UF1000,1000_QL80_.jpg"},
+      {title: "Star Wars VI", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUPQUMsWAY1ITLziual7yYeBW77wjvSeElqw28kDeXtc6PcXg_TT_vB4GDZu90MStjxx0&usqp=CAU"},
+    ]
   }
 
   cambiarTitulo = () => {
